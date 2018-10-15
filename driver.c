c4fcc68d7a4cddfcf7049ecade1477164059646b
Yoshiya <yohno@stuy.edu>
2018-10-15 14:32:52 -0400

    Just some slightly clearer examples

--- driver.c
+++ driver.c
@@ -4,17 +4,20 @@
 
 int main(){
   printf("\n");
-  struct node * l = NULL;
+  struct node *l = NULL;
  
-  l = insert(l, "y", "a");
-  l = insert(l, "-", "t");
+  l = insert(l, "b", "b");
+  l = insert(l, "b", "a");
   l = insert(l, "a", "a");
   l = insert(l, "a", "b");
-  l = insert(l, "a", "z");
-  l = insert(l, "r", "a");
-  l = insert(l, "fff", "ad");
-  l = insert(l, "d", "adf");
-  l = insert(l, "-", "a");
+  // l = insert(l, "-", "t");
+  // l = insert(l, "a", "a");
+  // l = insert(l, "a", "b");
+  // l = insert(l, "a", "z");
+  // l = insert(l, "r", "a");
+  // l = insert(l, "fff", "ad");
+  // l = insert(l, "d", "adf");
+  // l = insert(l, "-", "a");
  
   print_list(l);
   
