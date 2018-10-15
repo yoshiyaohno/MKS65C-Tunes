ad8674b836107e8db14fb649691d4af495c6a665
Yoshiya <yohno@stuy.edu>
2018-10-15 14:48:03 -0400

    Add find_artist() test cases'

--- driver.c
+++ driver.c
@@ -10,16 +10,20 @@ int main(){
   l = insert(l, "b", "a");
   l = insert(l, "a", "a");
   l = insert(l, "a", "b");
-  // l = insert(l, "-", "t");
-  // l = insert(l, "a", "a");
-  // l = insert(l, "a", "b");
-  // l = insert(l, "a", "z");
-  // l = insert(l, "r", "a");
-  // l = insert(l, "fff", "ad");
-  // l = insert(l, "d", "adf");
-  // l = insert(l, "-", "a");
+  l = insert(l, "-", "t");
+  l = insert(l, "a", "a");
+  l = insert(l, "a", "b");
+  l = insert(l, "a", "z");
+  l = insert(l, "r", "a");
+  l = insert(l, "fff", "ad");
+  l = insert(l, "d", "adf");
+  l = insert(l, "-", "a");
  
   print_list(l);
+
+  print_list( find_artist(l, "ad"));
+  print_list( find_artist(l, "b"));
+  print_list( find_artist(l, "t"));
   
   l = free_list(l);
 
